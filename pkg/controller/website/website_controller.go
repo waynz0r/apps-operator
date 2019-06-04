a91c3a1d92f085f64121bb186bd3e14ecd47a4b7
Zsolt Varga <zsolt.varga@console.hu>
2019-06-04 11:28:23 +0200

    Add Service and Ingress resources

--- pkg/controller/website/website_controller.go
+++ pkg/controller/website/website_controller.go
@@ -23,9 +23,11 @@ import (
 	appsv1alpha1 "github.com/waynz0r/apps-operator/pkg/apis/apps/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -70,9 +72,26 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create
-	// Uncomment watch a Deployment created by Website - change this for objects you create
+	// Watch for changes to a Deployment created by Website
 	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &appsv1alpha1.Website{},
+	})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to a Service created by Website
+	err = c.Watch(&source.Kind{Type: &corev1.Service{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &appsv1alpha1.Website{},
+	})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to an Ingress created by Website
+	err = c.Watch(&source.Kind{Type: &extensions.Ingress{}}, &handler.EnqueueRequestForOwner{
 		IsController: true,
 		OwnerType:    &appsv1alpha1.Website{},
 	})
@@ -114,42 +133,16 @@ func (r *ReconcileWebsite) Reconcile(request reconcile.Request) (reconcile.Resul
 		return reconcile.Result{}, err
 	}
 
-	// TODO(user): Change this to be the object type created by your controller
-	// Define the desired Deployment object
-	replicaCount := int32(1)
-	deploy := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name + "-deployment",
-			Namespace: instance.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicaCount,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:                     "nginx",
-							Image:                    "nginx",
-							ImagePullPolicy:          corev1.PullAlways,
-							TerminationMessagePath:   corev1.TerminationMessagePathDefault,
-							TerminationMessagePolicy: corev1.TerminationMessageReadFile,
-						},
-					},
-				},
-			},
-		},
-	}
-	if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	err = r.reconcile(deploy)
-	if err != nil {
-		return reconcile.Result{}, err
+	// Reconcile resources
+	for _, res := range []runtime.Object{
+		r.getDeployment(instance),
+		r.getService(instance),
+		r.getIngress(instance),
+	} {
+		err = r.reconcile(res)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
 	}
 
 	return reconcile.Result{}, nil
@@ -193,6 +186,12 @@ func (r *ReconcileWebsite) reconcile(desired runtime.Object) error {
 	case *appsv1.Deployment:
 		desired := desired.(*appsv1.Deployment)
 		desired.ResourceVersion = current.(*appsv1.Deployment).ResourceVersion
+	case *corev1.Service:
+		desired := desired.(*corev1.Service)
+		desired.ResourceVersion = current.(*corev1.Service).ResourceVersion
+	case *extensions.Ingress:
+		desired := desired.(*extensions.Ingress)
+		desired.ResourceVersion = current.(*extensions.Ingress).ResourceVersion
 	}
 
 	log.Info("update resource", logKeys...)
@@ -202,4 +201,95 @@ func (r *ReconcileWebsite) reconcile(desired runtime.Object) error {
 	}
 
 	return nil
+}
+
+func (r *ReconcileWebsite) getIngress(instance *appsv1alpha1.Website) runtime.Object {
+	resource := &extensions.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name + "-ingress",
+			Namespace: instance.Namespace,
+		},
+		Spec: extensions.IngressSpec{
+			Rules: []extensions.IngressRule{
+				{
+					Host: "demo1.k8smeetup.io",
+					IngressRuleValue: extensions.IngressRuleValue{
+						HTTP: &extensions.HTTPIngressRuleValue{
+							Paths: []extensions.HTTPIngressPath{
+								{
+									Backend: extensions.IngressBackend{
+										ServiceName: instance.Name + "-service",
+										ServicePort: intstr.FromInt(80),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	controllerutil.SetControllerReference(instance, resource, r.scheme)
+
+	return resource
+}
+
+func (r *ReconcileWebsite) getService(instance *appsv1alpha1.Website) runtime.Object {
+	resource := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name + "-service",
+			Namespace: instance.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Port:       80,
+					Protocol:   corev1.ProtocolTCP,
+					TargetPort: intstr.FromInt(80),
+				},
+			},
+			Selector: map[string]string{"deployment": instance.Name + "-deployment"},
+		},
+	}
+
+	controllerutil.SetControllerReference(instance, resource, r.scheme)
+
+	return resource
+}
+
+func (r *ReconcileWebsite) getDeployment(instance *appsv1alpha1.Website) runtime.Object {
+	replicaCount := int32(1)
+
+	resource := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name + "-deployment",
+			Namespace: instance.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicaCount,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:                     "nginx",
+							Image:                    "nginx",
+							ImagePullPolicy:          corev1.PullAlways,
+							TerminationMessagePath:   corev1.TerminationMessagePathDefault,
+							TerminationMessagePolicy: corev1.TerminationMessageReadFile,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	controllerutil.SetControllerReference(instance, resource, r.scheme)
+
+	return resource
 }
